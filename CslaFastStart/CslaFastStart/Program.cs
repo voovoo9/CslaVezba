0d54e25599ed09fc50d14057cc5651900ae949a7
Vukasin Vukotic <voovoo9@gmail.com>
2019-04-01 10:08:43 +0200

    #1 Dal, Dto, and Edit added
    PersonDal, dto and edit created, also some usiness logic

--- CslaFastStart/CslaFastStart/Program.cs
+++ CslaFastStart/CslaFastStart/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,63 @@ namespace CslaFastStart
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ceating a new person");
+            var person = Csla.DataPortal.Create<PersonEdit>();
+            Console.WriteLine("Enter first name: ");
+            person.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter last name: ");
+            person.LastName = Console.ReadLine();
+            if(person.IsSavable)
+            {
+                person = person.Save();
+                Console.WriteLine($"Added person with id {person.Id}. First name = '{person.FirstName}', last name = '{person.LastName}'.");
+            }
+            else
+            {
+                Console.WriteLine("invalid entery");
+                foreach (var item in person.BrokenRulesCollection)
+                    Console.WriteLine(item.Description);
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updating existing person");
+            person = Csla.DataPortal.Fetch<PersonEdit>(person.Id);
+            Console.Write($"Update last name [{person.LastName}]: ");
+            var temp = Console.ReadLine();
+            if(!string.IsNullOrWhiteSpace(temp))
+            {
+                person.FirstName = temp;
+            }
+            Console.WriteLine($"Update last name [{person.LastName}]: ");
+            temp = Console.ReadLine();
+            if(string.IsNullOrWhiteSpace(temp))
+            {
+                person.LastName = temp;
+            }
+            if(person.IsSavable)
+            {
+                person = person.Save();
+                Console.WriteLine($"Updated person with id {person.Id}. First name = '{person.FirstName}', last name = '{person.LastName}'.");
+            }
+            else
+            {
+                if(person.IsDirty)
+                {
+                    Console.WriteLine("Invalid entry");
+                    foreach(var item in person.BrokenRulesCollection)
+                        Console.WriteLine(item.Description);
+                    Console.ReadKey();
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("No changes, nothing to save.");
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ CslaFastStart/DataAccessLayer/PersonDal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class PersonDal
+    {
+        //stores data in inmemory static collection
+        //todo add database and data context
+        private static List<PersonDto> _list = new List<PersonDto>();
+
+        public PersonDto Create()
+        {
+            return new PersonDto { Id = -1 };
+        }
+
+        public PersonDto GetPerson(int id)
+        {
+            var entity = _list.FirstOrDefault(_ => _.Id == id);
+            if (entity == null)
+                throw new Exception("Index not found");
+            return entity;
+        }
+
+        public int InsertPerson(PersonDto data)
+        {
+            var newId = 1;
+            if (_list.Count > 0)
+                newId = _list.Max(_ => _.Id) + 1;
+            data.Id = newId;
+            _list.Add(data);
+            return newId;
+        }
+
+        public void UpdatePerson(PersonDto data)
+        {
+            var entity = GetPerson(data.Id);
+            entity.FirstName = data.FirstName;
+            entity.LastName = data.LastName;
+        }
+
+        public void DeletePeson(int id)
+        {
+            var entity = GetPerson(id);
+            _list.Remove(entity);
+        }
+    }
+}
